e9e21573bd42f7c6976dc4bd5eaa7eff4a38bf93
Alexksandro Fq <168040540+Alexksandrofq@users.noreply.github.com>
2024-11-08 16:09:29 -0300

    Update Inseto.java

--- src/main/java/com/atividade/Inseto.java
+++ src/main/java/com/atividade/Inseto.java
@@ -1,12 +1,12 @@
-package com.atividade;
+package com.atividade;//pacote
 
-public abstract class Inseto extends Animal {
-    public Inseto(String nome) {
-        super(nome);
+public abstract class Inseto extends Animal {//criação da classe abstrata Inseto que está herdando da classe Animal
+    public Inseto(String nome) {//construtor com um parâmetro
+        super(nome);//chama o construtor da classe Animal
     }
 
-    @Override
+    @Override//sobrescreve o método mover
     public void mover() {
-        System.out.println(nome + " está se movendo.");
+        System.out.println(nome + " está se movendo.");//mensagem de saída
     }
 }
