e70dec979abaacfe58fd4c828e47f6a9fadc9d8b
Alexksandro Fq <168040540+Alexksandrofq@users.noreply.github.com>
2024-11-08 16:01:36 -0300

    Update Cachorro.java

--- src/main/java/com/atividade/Cachorro.java
+++ src/main/java/com/atividade/Cachorro.java
@@ -1,17 +1,17 @@
-package com.atividade;
+package com.atividade;//pacote
 
-public class Cachorro extends Mamifero {
-    public Cachorro() {
-        super("Cachorro");
+public class Cachorro extends Mamifero {//criação da classe Cachorro que está herdando da classe Mamifero
+    public Cachorro() {//construtor
+        super("Cachorro");//chama o construtor da classe Mamifero, passando Cachorro como parâmetro, isso inicializa o nome do mamifero como "Cachorro" na classe base.
     }
 
-    @Override
+    @Override//sobrescreve o método rrespirar
     public void respirar() {
-        System.out.println("Cachorro respira.");
+        System.out.println("Cachorro respira.");//mensagem de saída
     }
 
     @Override
-    public void emitirSom() {
-        System.out.println("Cachorro está latindo.");
+    public void emitirSom() {//sobrescreve o método emitirSom
+        System.out.println("Cachorro está latindo.");//mensagem de saída
     }
 }
