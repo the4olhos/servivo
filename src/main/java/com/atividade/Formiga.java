eec8b91039abe8b5ba0eb9618b9dfdf3fc041ecb
Alexksandro Fq <168040540+Alexksandrofq@users.noreply.github.com>
2024-11-08 16:06:09 -0300

    Update Formiga.java

--- src/main/java/com/atividade/Formiga.java
+++ src/main/java/com/atividade/Formiga.java
@@ -1,17 +1,17 @@
-package com.atividade;
+package com.atividade;//pacote
 
-public class Formiga extends Inseto {
-    public Formiga() {
-        super("Formiga");
+public class Formiga extends Inseto {//criação da classe Formiga que está herdando da classe Inseto
+    public Formiga() {//construtor
+        super("Formiga");//chama o construtor da classe inseto, passando formiga como parâmetro. Isso inicializa o nome do inseto como "Formiga" na classe base.
     }
 
-    @Override
+    @Override//sobrescreve o método respirar
     public void respirar() {
-        System.out.println("Formiga respira.");
+        System.out.println("Formiga respira.");//mensagem de saída
     }
 
-    @Override
+    @Override//sobrescreve o método emitirSom
     public void emitirSom() {
-        System.out.println("Formiga emite um som.");
+        System.out.println("Formiga emite um som.");//mensagem de saída
     }
 }
