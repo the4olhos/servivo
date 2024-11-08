4bf87ed2a3b33af9599c963176b60d33313ee9ee
Alexksandro Fq <168040540+Alexksandrofq@users.noreply.github.com>
2024-11-08 16:11:44 -0300

    Update Jacare.java

--- src/main/java/com/atividade/Jacare.java
+++ src/main/java/com/atividade/Jacare.java
@@ -1,17 +1,17 @@
-package com.atividade;
+package com.atividade;//pacote
 
-public class Jacare extends Reptil {
-    public Jacare() {
-        super("Jacaré");
+public class Jacare extends Reptil {//criação da classe Jacare que está herdando da classe Reptil
+    public Jacare() {//construtor
+        super("Jacaré");//chama o construtor da classe Reptil, passando jacare como argumento. Isso inicializa o nome do reptil como "Jacaré" na classe base.
     }
 
-    @Override
+    @Override//sobrescreve o mpetodo respirar
     public void respirar() {
-        System.out.println("Jacaré respira.");
+        System.out.println("Jacaré respira.");//mensagem de saída
     }
 
-    @Override
+    @Override//sobrescreve o método emitirSom
     public void emitirSom() {
-        System.out.println("Jacaré emite um som.");
+        System.out.println("Jacaré emite um som.");//mesagem de saída
     }
 }
