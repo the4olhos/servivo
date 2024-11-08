71eba40f1e4f718b6c5dacf80764356a478e36c1
Alexksandro Fq <168040540+Alexksandrofq@users.noreply.github.com>
2024-11-08 16:21:10 -0300

    Update Mamifero.java

--- src/main/java/com/atividade/Mamifero.java
+++ src/main/java/com/atividade/Mamifero.java
@@ -1,12 +1,12 @@
-package com.atividade;
+package com.atividade;//pacote
 
-public abstract class Mamifero extends Animal {
-    public Mamifero(String nome) {
-        super(nome);
+public abstract class Mamifero extends Animal {//criação da classe abstrata Mamifero que está herdando da classe Animal
+    public Mamifero(String nome) {//construtor
+        super(nome);//chama o construtor da classe Animal
     }
 
-    @Override
+    @Override//sobrescreve o método mover
     public void mover() {
-        System.out.println(nome + " está correndo.");
+        System.out.println(nome + " está correndo.");//mensagem de saída
     }
 }
