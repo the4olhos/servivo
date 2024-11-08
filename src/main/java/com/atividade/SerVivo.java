0062f7b3a1968f44ffa84170272265432f76d9f4
Alexksandro Fq <168040540+Alexksandrofq@users.noreply.github.com>
2024-11-08 16:25:46 -0300

    Update SerVivo.java

--- src/main/java/com/atividade/SerVivo.java
+++ src/main/java/com/atividade/SerVivo.java
@@ -1,16 +1,16 @@
-package com.atividade;
+package com.atividade;//pacote
 
-public abstract class SerVivo implements Acoes {
-    protected String nome;
+public abstract class SerVivo implements Acoes {//criação da classe abstrata SerVico que está implementando a classe Acoes
+    protected String nome;//variável nome do tipo string que está protegida
 
-    public SerVivo(String nome) {
+    public SerVivo(String nome) {//construtor que inicializa com a variavel, com o valor passando como parâmetro 
         this.nome = nome;
     }
 
-    public abstract void respirar();
+    public abstract void respirar();//método abstrato respirar
 
-    @Override
+    @Override//sobrescreve o método emitirSom
     public void emitirSom() {
-        System.out.println(nome + " está emitindo um som.");
+        System.out.println(nome + " está emitindo um som.");//mensgaem dde saída
     }
 }
