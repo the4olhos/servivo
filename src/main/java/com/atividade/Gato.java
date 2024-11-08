9a0e351dee9b580347bad637dabf60c61cf84ca2
Alexksandro Fq <168040540+Alexksandrofq@users.noreply.github.com>
2024-11-08 16:07:54 -0300

    Update Gato.java

--- src/main/java/com/atividade/Gato.java
+++ src/main/java/com/atividade/Gato.java
@@ -1,17 +1,17 @@
-package com.atividade;
+package com.atividade;//pacote
 
-public class Gato extends Mamifero {
-    public Gato() {
-        super("Gato");
+public class Gato extends Mamifero {//criação da classe Gato que está herdando da classe Mamifero
+    public Gato() {//construtor
+        super("Gato");//chama o construtor da classe Mamifero, passando gato como parâmetro. Isso inicializa o nome do mamifero como "Gato" na classe base.
     }
 
-    @Override
+    @Override//sobrescreve o método respirar
     public void respirar() {
-        System.out.println("Gato respira.");
+        System.out.println("Gato respira.");//ensagem de saída
     }
 
-    @Override
+    @Override//sobrescreve o método emitirSom
     public void emitirSom() {
-        System.out.println("Gato está miando.");
+        System.out.println("Gato está miando.");//mensagem de saída
     }
 }
