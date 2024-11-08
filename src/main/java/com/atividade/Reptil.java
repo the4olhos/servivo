4ca7dcb680909764a74a6edbcfbf6a6594d03f03
Alexksandro Fq <168040540+Alexksandrofq@users.noreply.github.com>
2024-11-08 16:22:22 -0300

    Update Reptil.java

--- src/main/java/com/atividade/Reptil.java
+++ src/main/java/com/atividade/Reptil.java
@@ -1,12 +1,12 @@
-package com.atividade;
+package com.atividade;//pacote
 
-public abstract class Reptil extends Animal {
-    public Reptil(String nome) {
-        super(nome);
+public abstract class Reptil extends Animal {//criação da classe abstrata Reptil que esta herdando da classe Animal
+    public Reptil(String nome) {//construtor
+        super(nome);//chama o construtor da classe Animal
     }
 
-    @Override
+    @Override//sobrescreve o método mover
     public void mover() {
-        System.out.println(nome + " está se movendo.");
+        System.out.println(nome + " está se movendo.");//mensagem de saída
     }
 }
