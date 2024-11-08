eb0d7ccef63f7df75c99a81731b5995642bb2595
Alexksandro Fq <168040540+Alexksandrofq@users.noreply.github.com>
2024-11-08 16:27:27 -0300

    Update Ave.java

--- src/main/java/com/atividade/Ave.java
+++ src/main/java/com/atividade/Ave.java
@@ -1,11 +1,11 @@
-package com.atividade;//importação do pacote
+package com.atividade;//pacote
 
 public abstract class Ave extends Animal {//criação da classe abstrata Ave que está herdando da classe Animal
     public Ave(String nome) {//construtor com um parâmetro
-        super(nome);//chama o construtor da classe pai(SerVivo)
+        super(nome);//chama o construtor da classe Animal
     }
 
-    @Override//sobescreve o método mover
+    @Override//sobrescreve o método mover
     public void mover() {
         System.out.println(nome + " está se locomovendo.");//mensagem de saída
     }
