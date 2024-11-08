09352a8afa603580393087ad608063ebfb912ffc
Alexksandro Fq <168040540+Alexksandrofq@users.noreply.github.com>
2024-11-08 15:57:21 -0300

    Update Borboleta.java

--- src/main/java/com/atividade/Borboleta.java
+++ src/main/java/com/atividade/Borboleta.java
@@ -1,18 +1,18 @@
-package com.atividade;
+package com.atividade;//importação de pacote
 
-public class Borboleta extends Inseto {
-    public Borboleta() {
-        super("Borboleta");
+public class Borboleta extends Inseto {//criação da classe Borboleta que está herdando da classe Inseto
+    public Borboleta() {//construtor
+        super("Borboleta");//chama o construtor da classe pai(Inseto), passando "Borboleta" como argumento. Isso inicializa o nome do inseto como "Borboleta" na classe base.
     }
 
-    @Override
+    @Override//sobrescreve o método respirar
     public void respirar() {
-        System.out.println("Borboleta respira.");
+        System.out.println("Borboleta respira.");//mensagem de saída
     }
 
-    @Override
+    @Override//sobrescreve o método emitirSom
     public void emitirSom() {
-        System.out.println("Borboleta emite um som.");
+        System.out.println("Borboleta emite um som.");//mensagem de saída
     }
 }
 
