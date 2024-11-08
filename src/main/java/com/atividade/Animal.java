07fce76090b9454f0214828306d4d7b5ca9b7d14
Alexksandro Fq <168040540+Alexksandrofq@users.noreply.github.com>
2024-11-08 15:47:41 -0300

    Update Animal.java

--- src/main/java/com/atividade/Animal.java
+++ src/main/java/com/atividade/Animal.java
@@ -1,11 +1,11 @@
-package com.atividade;
+package com.atividade;//importação do pacote
 
-public abstract class Animal extends SerVivo {
-    public Animal(String nome) {
-        super(nome);
+public abstract class Animal extends SerVivo {//criação da classe abstrata Animal que está herdando da classe SerVivo
+    public Animal(String nome) {//construtor que tem um parâmetro
+        super(nome);//chama o construtor da classe pai (SerVivo)
     }
 
-    public void locomover() {
-        System.out.println(nome + " está se locomovendo.");
+    public void locomover() {//m~etodo locomover
+        System.out.println(nome + " está se locomovendo.");//mensagem de saída
     }
 }
